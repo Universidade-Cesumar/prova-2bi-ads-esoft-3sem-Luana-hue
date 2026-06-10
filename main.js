70adc8d02a5e107debb3b4ec997a198cc94be1cb
Luana Bueno <taynaluana89@gmail.com>
2026-06-10 19:57:07 -0300

    adicionando função listarmateriais

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 const URL_API =
 "https://6a29e4c4f59cb8f65f1db7d3.mockapi.io/materiais";
+
+async function listarMateriais() {
+    const resposta = await fetch(URL_API);
+    const materiais = await resposta.json();
+
+    const lista = document.getElementById("lista-materiais");
+
+    lista.innerHTML = "";
+
+    materiais.forEach(material => {
+        lista.innerHTML += `
+            <li>
+                ${material.nome} - Quantidade:
+                ${material.quantidade}
+            </li>
+        `;
+    });
+}
